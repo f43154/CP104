11ff536cb9f4a0f6c7f245fae1593b7770c85782
Fani H <fani43154@gmail.com>
2019-05-07 07:42:11 -0400

    Completed Assignment 4

--- Assignment04.py
+++ Assignment04.py
@@ -1,7 +1,8 @@
 '''
 Created on 2019 M04 18
 @author: Fani
-''''''
+Took about 2 hours to complete
+'''
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
 HOT_DAYS = 28
@@ -93,4 +94,80 @@ while total < min_budget and another == "Y":
 remaining = budget - total
 print("\nTotal spend on items: ${:.2f}".format(total))
 print("Amount remaining: ${:.2f}".format(remaining))
-'''
+
+# # # # # TASK 5 # # # # #
+print("\n# # # # # TASK 5 # # # # #")
+SENIOR_AGE = 65
+SENIOR_PRICE = 4.0
+
+ADULT_AGE = 18
+ADULT_PRICE = 5.0
+
+INFANT_AGE = 3
+INFANT_PRICE = 0
+
+STUDENT_AGE_LO = 10
+STUDENT_AGE_HI = 15
+STUDENT_PRICE = 1.0
+
+KID_PRICE = 2.0
+
+another = input("Purchase a ticket (Y/N)? ")
+age = 0
+total = 0
+while another == "Y":
+    age = int(input("How old are you? "))
+    if age >= SENIOR_AGE:
+        total += SENIOR_PRICE
+        print("The ticket costs ${:.2f}".format(SENIOR_PRICE))
+    elif age >= ADULT_AGE:
+        total += ADULT_PRICE
+        print("The ticket costs ${:.2f}".format(ADULT_PRICE))
+    elif STUDENT_AGE_HI >= age >= STUDENT_AGE_LO:
+        student = input("Are you a student at this school? (Y/N)? ")
+        if student == "Y":
+            total += STUDENT_PRICE
+            print("The ticket costs ${:.2f}".format(STUDENT_PRICE))
+        else:
+            total += KID_PRICE
+            print("The ticket costs ${:.2f}".format(KID_PRICE))
+    elif age >= INFANT_AGE:
+        total += KID_PRICE
+        print("The ticket costs ${:.2f}".format(KID_PRICE))
+    else:
+        total += INFANT_PRICE
+        print("The ticket costs ${:.2f}".format(INFANT_PRICE))
+
+    another = input("Purchase another ticket (Y/N)? ")
+    
+print("Total tickets price: ${:.2f}".format(total))
+
+# # # # # TASK 6 # # # # #
+print("\n# # # # # TASK 6 # # # # #")
+
+# Imports
+from random import randint
+
+numQuestions = 0
+numCorrect = 0
+another = "Y"
+
+while another == "Y":
+    randOne = randint(0, 999)
+    randTwo = randint(0, 999)
+    print("\n{:>6}".format(randOne))
+    print("+ {:>4}".format(randTwo))
+    userEquals = int(input("= "))
+    actualEquals = randOne + randTwo
+    
+    if userEquals == actualEquals:
+        print("Congratulations!\n")
+        numCorrect += 1
+        numQuestions += 1
+    else:
+        print("Sorry, the correct answer is {}\n".format(actualEquals))
+        numQuestions += 1
+    
+    another = input("Continue (Y/N): ")
+    
+print("\nYour final score is {}/{}".format(numCorrect, numQuestions))
