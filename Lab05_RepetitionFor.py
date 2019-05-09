e4470f040fb4a19eeb507fcc2cac55d4fad0fc67
Fani H <fani43154@gmail.com>
2019-05-09 07:28:00 -0400

    Completed Lab 5

--- Lab05_RepetitionFor.py
+++ Lab05_RepetitionFor.py
@@ -1,7 +1,8 @@
 '''
 Created on 2019 M05 7
 @author: Fani
-
+Completed in ~3.75 hours
+'''
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
 n = int(input("Enter n: "))
@@ -131,19 +132,70 @@ print("\n# # # # # TASK 11 # # # # #")
 width = int(input("Enter a number: "))
 for i in range(1, width + 1, 1):
     print((width - i) * ' ' + i * '#' + (i - 1) * '#')
-'''
+
 # # # # # TASK 12 # # # # #
 print("\n# # # # # TASK 12 # # # # #")
 width = int(input("Enter a number: "))
-last = 1
 
-for i in range(1, width + 1, 1):
+print("#")
+
+for i in range(2, width, 1):
+    print('#' + (i - 2) * ' ' + '#')
     
-    if i < 4:
-        last = i
-    else:
-        last = 0
-        
-    print("#" + (i - 2) * " " + (last - 1) * "#")
+if width >= 2:
+    print(width * "#")
 
-print("\n" + width * "#")
+# # # # # TASK 13 # # # # #
+print("\n# # # # # TASK 13 # # # # #")
+NUM_DAYS = 8
+num = int(input("Number of servers: "))
+day = 1
+total = 0
+
+while day < NUM_DAYS:
+    print("Day {}".format(day))
+    day += 1
+    for i in range(1, num + 1, 1):
+        tips = float(input("Tips for server {}: ".format(i)))
+        total += tips
+
+average = total / num
+print("\nTips per server: ${:.2f}".format(average))    
+
+# # # # # TASK 14 # # # # #
+print("\n# # # # # TASK 14 # # # # #")
+num = int(input("Number of IAs: "))
+week = 1
+markingHours = 0
+total = 0
+
+while week < 7:
+    print("\nWeek {}".format(week))
+    week += 1
+    
+    for i in range(1, num + 1, 1):
+        markingHours = float(input("Marking hours for IA {}: ".format(i)))
+        total += markingHours
+        
+print("\nTotal marking hours for all IAs: {:.2f}".format(total))
+
+# # # # # TASK 15 # # # # #
+print("\n# # # # # TASK 15 # # # # #")
+minBase = int(input("Enter minimum value of base: "))
+maxBase = int(input("Enter maximum value of base: "))
+incBase = int(input("Enter increment in base value: "))
+minHeight = int(input("Enter minimum value of height: "))
+maxHeight = int(input("Enter maximum value of height: "))
+incHeight = int(input("Enter increment in height value: "))
+cross = 0
+inertia = 0
+modulus = 0
+
+print("\n" + 14 * " " + "Cross-Sectional  Moment of  Section\n" + "Base  Height  Area" + 13 * " " + "Inertia    Modulus\n")
+
+for base in range(minBase, maxBase + 1, incBase):
+    for height in range(minHeight, maxHeight + 1, incHeight):
+        cross = base * height
+        inertia = (base * (height ** 3)) / 12
+        modulus = (base * (height ** 2)) / 6
+        print("{:>4}  x {:>4} {:>13.2f}  {:>7.2f}  {:>7.2f}".format(base, height, cross, inertia, modulus))
