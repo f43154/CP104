23699f11412726056b2e1c7460133b0ad6bc734d
Fanda <fani43154@gmail.com>
2019-06-14 08:53:00 -0400

    Completed Assignment 8

--- Assignment08.py
+++ Assignment08.py
@@ -5,12 +5,15 @@ Author: Fani
 ID: 123456789
 Email: email@mylaurier.ca
 @author: Fani
-'''
+Completed in 2 hours 45 mins
+
+from _ast import Num
 '''
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
 
 from random import choices
+from random import randint
 
 
 def bag_to_set(bag):
@@ -21,14 +24,12 @@ def bag_to_set(bag):
     return new_set
 
 
-bag = choices(range(10), k=5)
+bag = choices(range(5), k=10)
 print("Bag: ", bag)
 print("Set: ", bag_to_set(bag))
 
 # # # # # TASK 2 # # # # #
 print("\n# # # # # TASK 2 # # # # #")
-
-from random import choices
 
 
 def clean_list(values):
@@ -63,8 +64,6 @@ two_element_subset(string)
 
 # # # # # TASK 4 # # # # #
 print("\n# # # # # TASK 4 # # # # #")
-
-from random import randint
 
 
 def set_trials(subjects):
@@ -121,14 +120,65 @@ def license_test(correct_answers, student_answers):
     print("Indexes of incorrect answers: ", indexes)
 
 
-from random import choices
 correct_answers = choices(range(3), k=5)
 student_answers = choices(range(3), k=5)
 print("Correct Answers: ", correct_answers)
 print("Student Answers: ", student_answers)
 license_test(correct_answers, student_answers)
 
-'''
 # # # # # TASK 6 # # # # #
 print("\n# # # # # TASK 6 # # # # #")
 
+
+def categorize_accidents(city_counts):
+    for i in range(0, len(city_counts), 1):
+        if city_counts[i] >= 400:
+            accidents[4] += 1
+        elif city_counts[i] > 300:
+            accidents[3] += 1
+        elif city_counts[i] > 200:
+            accidents[2] += 1
+        elif city_counts[i] > 100:
+            accidents[1] += 1
+        elif city_counts[i] > 0:
+            accidents[0] += 1
+    return accidents
+
+
+city_counts = choices(range(500), k=20)
+print("City Accident Counts: ", city_counts)
+accidents = [0, 0, 0, 0, 0]
+categorize_accidents(city_counts)
+print("Accidents Categorized: ", accidents)
+print("\nCategory\tCounts\n< 100\t\t{}\n< 200\t\t{}\n< 300\t\t{}\n< 400\t\t{}\n400 or more\t{}".format(accidents[0], accidents[1], accidents[2], accidents[3], accidents[4]))
+
+# # # # # TASK 7 # # # # #
+print("\n# # # # # TASK 7 # # # # #")
+
+
+def num_to_text(num):
+    tens = 0
+    for i in range(0, 100, 1):
+        if i < 10:
+            print("{:>2} - {}".format(numbers[i], ONES[i]))
+        elif i < 20:
+            print("{:>2} - {}".format(numbers[i], TEENS[i - 10]))
+        elif i % 10 == 0:
+            print("{:>2} - {}ty".format(numbers[i], TENS[tens]))    
+            tens += 1
+            ones = 1
+        else:
+            tens -= 1
+            print("{:>2} - {}ty-{}".format(numbers[i], TENS[tens], ONES[ones]))
+            ones += 1
+            tens += 1
+
+
+ONES = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+TEENS = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+TENS = ["twen", "thir", "for", "fif", "six", "seven", "eigh", "nine"]
+
+numbers = []
+for i in range(0, 100, 1):
+    numbers.append(i)
+num_to_text(numbers)
