ce1f4fe629881361c7b92bf2c7a2cb9960814e4c
Fanda <fani43154@gmail.com>
2019-07-11 09:01:48 -0400

    Completed Task 1 of Lab 9
    Only took absolutely forever to understand files

--- Lab09_Files.py
+++ Lab09_Files.py
@@ -5,24 +5,35 @@ Author: Fani
 ID: 123456789
 Email: email@mylaurier.ca
 @author: Fani
+
+Completed Task 1 in....far too long, took time off to commute for new job and eventually move to new home
 '''
 
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
 
 
-def get_customer_record(file_handle, n): 
-    file_handle = open("customers.txt", "r")   
-    mylist = []
-    mylist.append(file_handle.readlines()[n])
-    
-    if mylist[0][:1].isalnum() == True:
-        print(mylist)
-    else:
+def customer_records(fv, n):
+    my_list = open(fv).read()
+    my_list = re.split(",|\n", my_list)
+    my_nested_list = []
+    start = 0
+    end = 5
+    num_lines = int(len(my_list) / 5)
+    i = 0
+    while i < num_lines:
+        my_nested_list.append(my_list[start:end])
+        start += 5
+        end += 5
+        i += 1
+    if n >= 5:
         print("[]")
-    file_handle.close
+    else:
+        print(my_nested_list[n])
 
 
-n = int(input("Enter a record number: "))
-file_handle = open("customers.txt", "r")  
-get_customer_record(file_handle, n)
+import re
+fv = "customers.txt"
+print("Find record n")
+n = int(input("Find a record number:  "))
+customer_records(fv, n)
