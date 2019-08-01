6b00561f327863c0e68dbb0bb753c5cc7f762455
Fanda <fani43154@gmail.com>
2019-08-01 07:22:26 -0400

    Completed Lab 9

--- Lab09_Files.py
+++ Lab09_Files.py
@@ -6,11 +6,11 @@ ID: 123456789
 Email: email@mylaurier.ca
 @author: Fani
 
-Completed Task 1 in....far too long, took time off to commute for new job and eventually move to new home
+Completed in....far too long, took time off to commute for new job and eventually move to new home
 '''
-from cgitb import small
-from pickle import APPEND
-'''
+from shutil import copyfile
+import re
+
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
 
@@ -39,7 +39,6 @@ def customer_records(fv, n):
         print(my_nested_list[n])
 
 
-import re
 fv = "customers.txt"
 print("Find record n")
 n = int(input("Find a record number:  "))
@@ -47,7 +46,6 @@ customer_records(fv, n)
 
 # # # # # TASK 2 # # # # #
 print("\n# # # # # TASK 2 # # # # #")
-
 
 
 def customer_by_id(fv, id_number):
@@ -73,9 +71,7 @@ def customer_by_id(fv, id_number):
         print(my_list[my_index:my_index + 5])
     else:
         print("[]")
-
     
-import re
 
 fv = "customers.txt"
 print("Find customer by id number")
@@ -115,9 +111,7 @@ def customer_by_id(fv):
             my_index = i
         i += 1
     print(my_nested_list[my_index])
-
     
-import re
 
 fv = "customers.txt"
 print("FInd customer with largest balance:")
@@ -154,9 +148,7 @@ def customer_by_id(fv):
             my_index = i
         i += 1
     print(my_nested_list[my_index])
-
     
-import re
 
 fv = "customers.txt"
 print("Find customer with earliest sign-in:")
@@ -182,13 +174,11 @@ def customer_by_id(fv, fields):
         start += 5
         end += 5
         i += 1
-    print(my_nested_list)
+    # print(my_nested_list)
     my_nested_list.append(fields)
     print("data appended to file")
-    print(my_nested_list)
-
+    # print(my_nested_list)
     
-import re
 
 fv = "customers.txt"
 fields = input("Data:")
@@ -259,6 +249,136 @@ f = open(fv, "r+")
 if not f.closed:
     print("file '{}' open for reading and writing".format(fv))
     append_increment(fv)
-'''
+
 # # # # # TASK 9 # # # # #
 print("\n# # # # # TASK 9 # # # # #")
+
+
+def count_frequency_value(fv, value):
+    my_list = open(fv).read()
+    my_list = [int(n) for n in my_list.split()]
+    count = 0
+    for i in range(0, len(my_list), 1):
+        if my_list[i] == value:
+            count += 1
+    print("{} appears {} time(s)".format(value, count))
+
+
+fv = "numbers.txt"
+f = open(fv, "r+")
+if not f.closed:
+    print("file '{}' is open for reading".format(fv))
+    value = int(input("Value to count: "))
+    count_frequency_value(fv, value)
+
+# # # # # TASK 10 # # # # #
+print("\n# # # # # TASK 10 # # # # #")
+
+
+def count_frequency_word(fv, word):
+    my_list = open(fv).read()
+    my_list = [n for n in my_list.split()]
+    count = 0
+    for i in range(0, len(my_list), 1):
+        if word == my_list[i]:
+            count += 1
+    print("'{}' appears {} time(S)".format(word, count))
+
+
+fv = "words.txt"
+f = open(fv, "r+")
+if not f.closed:
+    print("file '{}' is open for reading".format(fv))
+    word = input("Word to count: ")
+    count_frequency_word(fv, word)
+
+# # # # # TASK 11 # # # # #
+print("\n# # # # # TASK 11 # # # # #")
+
+
+def find_longest(fv):
+    my_list = open(fv).read()
+    my_list = [n for n in my_list.split()]
+    word = ""
+    for i in range(0, len(my_list), 1):
+        if len(word) <= len(my_list[i]):
+            word = my_list[i]
+    print("'{}' is the last longest word".format(word))
+
+
+fv = "words.txt"
+f = open(fv, "r+")
+if not f.closed:
+    print("file '{}' is open for reading".format(fv))
+    find_longest(fv)
+
+# # # # # TASK 12 # # # # #
+print("\n# # # # # TASK 12 # # # # #")
+
+
+def find_shortest(fv):
+    my_list = open(fv).read()
+    my_list = [n for n in my_list.split()]
+    word = my_list[0]
+    for i in range(len(my_list) - 1, -1, -1):
+        if len(word) >= len(my_list[i]):
+            word = my_list[i]
+    print("'{}' is the first shortest word".format(word))
+
+
+fv = "words.txt"
+f = open(fv, "r+")
+if not f.closed:
+    print("file '{}' is open for reading".format(fv))
+    find_shortest(fv)
+
+# # # # # TASK 13 # # # # #
+print("\n# # # # # TASK 13 # # # # #")
+
+
+def delete_line(fv, n):
+    my_list = open(fv).read()
+    my_list = [n for n in my_list.split()]
+    print("Line {} deleted:".format(n))
+    print(my_list[n])
+    my_list.pop(n)
+
+
+fv = "words.txt"
+f = open(fv, "r+")
+if not f.closed:
+    print("file '{}' is open for reading".format(fv))
+    n = int(input("Enter line number to delete: "))
+    delete_line(fv, n)
+
+# # # # # TASK 14 # # # # #
+print("\n# # # # # TASK 14 # # # # #")
+
+
+def file_copy(fv_1, fv_2):
+    copyfile(fv_1, fv_2)
+    print("Copying '{}' to '{}'".format(fv_1, fv_2))
+
+
+fv_1 = "words.txt"
+fv_2 = "new_words.txt"
+open(fv_2, "w+")
+file_copy(fv_1, fv_2)
+
+# # # # # TASK 15 # # # # #
+print("\n# # # # # TASK 15 # # # # #")
+
+
+def file_copy_n(fv_1, fv_2, n):
+    fv_1_list = open(fv_1).read()
+    fv_1_list = [n for n in fv_1_list.split()]
+    fv_2_list = open(fv_2).read()
+    fv_2_list = [n for n in fv_2_list.split()]
+    fv_2_list.append(fv_1_list[0:n])
+
+
+fv_1 = "words.txt"
+fv_2 = "new_words.txt"
+open(fv_2, "w+")
+n = int(input("Number of lines to copy: "))
+file_copy_n(fv_1, fv_2, n)
---
+++ Lab09_Files_Testing.py
@@ -0,0 +1,74 @@
+'''
+Created on 01 08 2019
+__updated__ = ''
+Author: Fani
+ID: 123456789
+Email: email@mylaurier.ca
+@author: Fani
+'''
+
+# # # # # TASK 1 # # # # #
+Find record n
+Find a record number:  3
+['43564', 'Weilin', 'Zhao', '450.25', '1998-01-03']
+
+# # # # # TASK 2 # # # # #
+Find customer by id number
+Enter an ID: 23456
+['23456', 'Alice', 'Smith', '1200.50', '1998-02-20']
+
+# # # # # TASK 3 # # # # #
+FInd customer with largest balance:
+['23456', 'Alice', 'Smith', '1200.50', '1998-02-20']
+
+# # # # # TASK 4 # # # # #
+Find customer with earliest sign-in:
+['43564', 'Weilin', 'Zhao', '450.25', '1998-01-03']
+
+# # # # # TASK 5 # # # # #
+Data:['35612',"David','Brown',237.56,'2008-10-10']
+data appended to file
+
+# # # # # TASK 6 # # # # #
+Smallest: 0
+Largest: 13
+Total: 113.00
+Average: 7.06
+
+# # # # # TASK 7 # # # # #
+file 'numbers.txt' open for reading and writing
+13 is appended
+
+# # # # # TASK 8 # # # # #
+file 'numbers.txt' open for reading and writing
+14 is appended
+
+# # # # # TASK 9 # # # # #
+file 'numbers.txt' is open for reading
+Value to count: 9
+9 appears 2 time(s)
+
+# # # # # TASK 10 # # # # #
+file 'words.txt' is open for reading
+Word to count: Exercise
+'Exercise' appears 3 time(S)
+
+# # # # # TASK 11 # # # # #
+file 'words.txt' is open for reading
+'Education' is the last longest word
+
+# # # # # TASK 12 # # # # #
+file 'words.txt' is open for reading
+'Hello' is the first shortest word
+
+# # # # # TASK 13 # # # # #
+file 'words.txt' is open for reading
+Enter line number to delete: 7
+Line 7 deleted:
+Halloween
+
+# # # # # TASK 14 # # # # #
+Copying 'words.txt' to 'new_words.txt'
+
+# # # # # TASK 15 # # # # #
+Number of lines to copy: 5
