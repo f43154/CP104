28864ff91e2671b12db2340b23c1e73ee8c05234
Fanda <fani43154@gmail.com>
2019-08-19 07:38:05 -0400

    Finished Assignment 9

--- Assignment09.py
+++ Assignment09.py
@@ -8,9 +8,10 @@ Email: email@mylaurier.ca
 
 Completed Task 1 and 2 in 30 mins
 '''
-'''
+
 # # # # # TASK 1 # # # # #
 print("\n# # # # # TASK 1 # # # # #")
+from test.test_threading_local import target
 import re
 
 
@@ -35,8 +36,6 @@ file_head(fh, n)
 # # # # # TASK 2 # # # # #
 print("\n# # # # # TASK 2 # # # # #")
 
-import re
-
 
 def number_lines(fh_in, fh_out):
     f = open(fh_in).read()
@@ -56,8 +55,6 @@ number_lines(fh_in, fh_out)
 
 # # # # # TASK 3 # # # # #
 print("\n# # # # # TASK 3 # # # # #")
-
-import re
 
 
 def get_addresses(fh):
@@ -84,10 +81,6 @@ get_addresses(fh)
 
 # # # # # TASK 4 # # # # #
 print("\n# # # # # TASK 4 # # # # #")
-
-
-from test.test_threading_local import target
-import re
 
 
 def merge_letters(fh_letter, fh_addresses, fh_merged):
@@ -124,11 +117,8 @@ fh_addresses = "addresses.txt"
 fh_merged = "merged_letters.txt"
 merge_letters(fh_letter, fh_addresses, fh_merged)
 
-
 # # # # # TASK 5 # # # # #
 print("\n# # # # # TASK 5 # # # # #")
-
-import re
 
 
 def student_info(students):
@@ -209,6 +199,45 @@ print("Input file:", fh_in)
 print("Output file:", fh_out)
 ciphertext = input("Enter ciphertext:")
 substitute(fh_in, fh_out, ciphertext)
-'''
+
 # # # # # TASK 7 # # # # #
 print("\n# # # # # TASK 7 # # # # #")
+
+
+def shift(fh_in, fh_out, n):
+    print()  # Leave a space
+    message = open(fh_in).read()  # Import message from pelee.txt as a list
+    message = message.upper()
+    message_list = [char for char in message]
+    
+    alphabet = "abcDEFGHIJKLMNOPQRSTUVWXYZ"  # Create alphabet as list
+    alphabet = [char for char in alphabet.upper()]
+#     print("alphabet\t", alphabet)
+    
+    shifted_alphabet = []  # Create shifted alphabet
+    for i in range(0, len(alphabet) - n, 1):
+        shifted_alphabet.append(alphabet[i + n])
+    for i in range(0, abs(len(alphabet) - len(shifted_alphabet)), 1):
+        shifted_alphabet.append(alphabet[i])
+#     print("shifted alpha\t", shifted_alphabet)
+
+    new_message_list = []
+    for i in range(0, len(message_list), 1):
+        if message_list[i] in alphabet:
+            new_message_list.append(shifted_alphabet[alphabet.index(message_list[i])])
+        else:
+            new_message_list.append(message_list[i])
+    new_message = ''.join(new_message_list)  # Re-assemble new message list into one string
+#     print("message\t\t", message)
+#     print("new message\t", new_message)
+    
+    with open(fh_out, "a") as outfile:
+        outfile.write(new_message)
+
+
+fh_in = "pelee.txt"
+fh_out = "pelee_shift.txt"
+print("Input file:", fh_in)
+print("Output file:", fh_out)
+n = int(input("Enter shift: "))
+shift(fh_in, fh_out, n)
