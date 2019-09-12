62169ee49f4bbc285ed9e32e82b5f74bd45ad0e6
Fanda <fani43154@gmail.com>
2019-09-12 08:14:20 -0400

    Stuck on Task 1
    Earliest occurence of a number

--- Assignment10.py
+++ Assignment10.py
@@ -12,23 +12,34 @@ print("\n# # # # # TASK 1 # # # # #")
 
 
 def binary_search(values, key):
-    if values[int(len(values) / 2)] > key:
-        print("if bigger", int(len(values) / 2))
-        if values[int(len(values) - (len(values) / 2))] > key:
-            print("if bigger, big sublist, bigger", int(len(values) - (len(values) / 2)))
-        if values[int(len(values) - (len(values) / 2))] <= key:
-            print("if bigger, big sublist, smaller", int(len(values) - (len(values) / 2)))
-    elif values[int(len(values) / 2)] <= key:
-        print("if smaller equal", int(len(values) / 2))
-        if values[int((len(values) / 2) / 2)] > key:
-            print("if smaller equal, small sublist, bigger", int((len(values) / 2) / 2))
-        if values[int((len(values) / 2) / 2)] <= key:
-            print("if smaller equal, small sublist, smaller", int((len(values) / 2) / 2))
-    else:
-        print("else")
+    my_list = list(values)
+    print("list(values) ", list(values))
+    print()
+    
+    while(len(my_list) > 1):  # Stop when my_list is just the key
+        if len(my_list) % 2 == 0:  # if length of list is even
+            print("\n// list is even lengthed //\n")
+            middle = int(len(my_list) / 2)
+            if key <= my_list[middle]:
+                print("key <= my_list[middle]")
+                my_list = my_list[0:middle]
+                print("new my_list: \t", my_list)
+                
+            elif key > my_list[middle]:
+                print("key > my_list[middle]")
+                my_list = my_list[middle:len(my_list)]
+                print("new my_list: \t", my_list)
+                
+        elif len(my_list) % 2 == 1:
+            print("my_list is odd lengthed: ", len(my_list))
+
+    print("\nfinal my_list", my_list)
 
     
-values = [2, 5, 5, 5, 5, 5, 5, 8]
+values = [1, 2, 3, 4, 5, 6, 7, 8]  
+# values = [2, 5, 5, 5, 5, 5, 5, 8]
 print("Search: {} ".format(values), end="")
-key = int(input("for "))
+key = 4
+print("for", key)
+# key = int(input("for "))
 binary_search(values, key)
